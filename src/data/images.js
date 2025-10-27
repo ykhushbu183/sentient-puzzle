22599e5b72c94ea24c21b43ee694f85a2071ba05
Tushar yadav <132874534+ykhushbu183@users.noreply.github.com>
2025-10-28 00:29:48 +0530

    Update images.js

--- src/data/images.js
+++ src/data/images.js
@@ -1,1 +1,13 @@
+export const images = [
+  '/images/img1.png',
+  '/images/img2.png',
+  '/images/img3.png',
+  '/images/img4.png',
+  '/images/img5.png',
+  '/images/img6.png',
+  '/images/img7.png',
+  '/images/img8.png',
+  '/images/img9.png',
+  '/images/img10.png'
+];
 
