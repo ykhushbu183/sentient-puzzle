0a666972d71afa4e807005a8d665fdeab5359071
Tushar yadav <132874534+ykhushbu183@users.noreply.github.com>
2025-10-27 23:00:39 +0530

    Create PuzzleGame.jsx

---
+++ src/components/PuzzleGame.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PuzzleGame = () => {
+  return (
+    <div className="puzzle-container">
+      <h2>Puzzle Game Coming Soon</h2>
+    </div>
+  );
+};
+
+export default PuzzleGame;
