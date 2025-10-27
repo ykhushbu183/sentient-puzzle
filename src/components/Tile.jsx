cab7221bd23c7fc99d3ac2f22b794a1be8be3ab3
Tushar yadav <132874534+ykhushbu183@users.noreply.github.com>
2025-10-27 21:53:19 +0530

    Update Tile.jsx

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+
+export default function Tile({ piece, onClick }) {
+  return (
+    <div
+      className="tile"
+      style={{ backgroundImage: `url(${piece.img})`, backgroundPosition: piece.pos }}
+      onClick={onClick}
+    />
+  )
+}
 
